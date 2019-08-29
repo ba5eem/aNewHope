9e5dd466243185f0bca509769504b35805728827
baseem <baseem@gmail.com>
2019-08-29 09:52:35 -1000

    add a new user - required password change on first login

--- newuser.sh
+++ newuser.sh
@@ -8,7 +8,7 @@ function ctrl_c() {
 trap ctrl_c INT
 
 # start editing your bash script here
-sudo useradd -m -p $(openssl passwd -1 kenobi) $1 -s /bin/bash
+sudo useradd -m -p $(openssl passwd -1 $2) $1 -s /bin/bash
 echo .........
 sudo mkdir /home/$1/.ssh
 sudo touch /home/$1/.ssh/authorized_keys
@@ -16,6 +16,6 @@ sudo chown -R $1:$1 /home/$1/.ssh
 sudo usermod -aG sudo $1
 sudo chage -d 0 $1
 echo Username is: $1
-echo User created your default password is: kenobi
+echo User created your default password is: $2
 echo you will be required to change the password on first login
 echo script complete - Aloha!
