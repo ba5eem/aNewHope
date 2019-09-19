ef93337d58eeff0f988ac9fc06ba8b3e8fdef4c6
Baseem <baseem@gmail.com>
2019-09-19 09:02:09 -0500

    Updated to LTS docker-compose

--- installDocker_DockerCompose.sh
+++ installDocker_DockerCompose.sh
@@ -18,6 +18,6 @@ sudo usermod -aG docker ${USER} &&
 su - ${USER} &&
 
 # install docker - compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose &&
 docker-compose --version
