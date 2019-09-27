8964395a6afeece431e3339452b7d2440169cebd
baseem <baseem@gmail.com>
2019-09-27 09:54:18 -0500

    project update

--- i_nvm.sh
+++ i_nvm.sh
@@ -9,6 +9,4 @@ trap ctrl_c INT
 
 # start editing your bash script here
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash &&
-source ~/.bashrc &&
-nvm install 8
 echo 'nvm: success'
