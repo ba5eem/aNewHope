5b9a401efb9711e3045b14b181e3ad4b6b191f57
baseem <baseem@gmail.com>
2019-09-27 09:58:31 -0500

    project update

--- setup.sh
+++ setup.sh
@@ -10,6 +10,6 @@ trap ctrl_c INT
 # start editing your bash script here
 curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/newuser.sh | bash -s $1 $2 && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_docker.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_dockercompose.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_go.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_nvm.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/add_docker_sudo.sh | bash &&
 source ~/.bashrc &&
-nvm install 8 &&
 echo 'setup complete'
+&& nvm install 8
 
