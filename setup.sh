ae0d8e6f0fb49e59a9769c0b49df64d3a314db48
baseem <baseem@gmail.com>
2019-09-27 10:13:35 -0500

    project update

--- setup.sh
+++ setup.sh
@@ -9,5 +9,8 @@ trap ctrl_c INT
 
 # start editing your bash script here
 curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/newuser.sh | bash -s $1 $2 && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_docker.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_dockercompose.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_go.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/i_nvm.sh | bash && curl -s https://raw.githubusercontent.com/ba5eem/aNewBash/master/add_docker_sudo.sh | bash &&
-echo 'setup complete'
+echo 'setup complete: success' &&
+echo Please enter NODE version you want installed?
+read NODE
+nvm install $NODE
 
