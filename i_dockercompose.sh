4fb3218965fc1f9274727ab38a4855c0f5766e83
baseem <baseem@gmail.com>
2019-09-27 09:44:12 -0500

    project update

--- i_dockercompose.sh
+++ i_dockercompose.sh
@@ -11,5 +11,4 @@ trap ctrl_c INT
 
 # install docker - compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose &&
-sudo chmod +x /usr/local/bin/docker-compose &&
-docker-compose --version
+sudo chmod +x /usr/local/bin/docker-compose
