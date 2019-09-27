ebff63477f4bd421035f0f3945bbfcc334022957
baseem <baseem@gmail.com>
2019-09-27 09:40:07 -0500

    project update

--- i_go.sh
+++ i_go.sh
@@ -8,7 +8,6 @@ function ctrl_c() {
 trap ctrl_c INT
 
 # start editing your bash script here
-cd - &&
 curl -O https://dl.google.com/go/go1.10.3.linux-amd64.tar.gz &&
 tar xvf go1.10.3.linux-amd64.tar.gz &&
 sudo chown -R root:root ./go &&
